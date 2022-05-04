2f44aa7ec1762133fd5dba2827ddf381da50b401
Eugene <evgeshka120@gmail.com>
2022-05-04 21:13:54 +0300

    constants and variables

--- main.py
+++ main.py
@@ -13,11 +13,34 @@ surface = pygame.display.set_mode(RES)
 clock = pygame.time.Clock()
 draw_options = pymunk.pygame_util.DrawOptions(surface)
 
+# PyMunk variables
+
+space = pymunk.Space()
+space.gravity = 0, 8000
+
+# PyMunk constants
+
+E = 1.602 * 10 ** -19  # electic charge
+M0 = 9.109 * 10 ** -31  # electron's mass
+C = 3 * 10 ** 8  # light speed
+EC0 = 8.854 * 10 ** -12  # dielectric constant
+PI = 3.14
+
+# Electron
+
+e_shape = pymunk.shapes.Circle(E**2/(4*PI*EC0*M0*C**2))
+
+# Drawing
+
 while True:
     surface.fill(pygame.Color('white'))
 
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             exit()
+
+    space.step(1 / FPS)
+    space.debug_draw(draw_options)
+
     pygame.display.flip()
     clock.tick(FPS)
